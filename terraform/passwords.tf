1a1f173838901520a924f8538f1c9d4488f01d69
Krishnasis Mandal <krishnasis@hotmail.com>
2025-04-05 19:44:14 +0200

    fix: Depends on

--- terraform/passwords.tf
+++ terraform/passwords.tf
@@ -4,7 +4,7 @@ resource "random_password" "main" {
 }
 
 resource "azurerm_key_vault_secret" "app_password" {
-  depends_on   = [azurerm_role_assignment.current_sp_to_kv_contributor, azurerm_key_vault_access_policy.current_sp_to_kv_contributor]
+  depends_on   = [azurerm_role_assignment.main, azurerm_key_vault_access_policy.main]
   name         = "${var.ad_app_name}-password"
   value        = tolist(azuread_application.main.password).0.value
   key_vault_id = azurerm_key_vault.main.id
